5c0617c580f586c208e67ef2f9299adee86eefba
aabhassinghal <aabhassi@buffalo.edu>
2018-02-22 01:01:30 -0500

    usage of state aswell

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Header from "./Header"
 import Footer from "./Footer";
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {title:"Aabhas is not here"};
+    }
     render() {
+        setTimeout(()=>{this.setState({title:"Aabhas is here"});},2000)
         const title = "Aabhas is here";
         return (
             <div className="App">
-                <Header title={title}/>
+                <Header title={this.state.title}/>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1 className="App-title">Welcome to React Aabhas</h1>
                 <Footer/>
